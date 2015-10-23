da9ba5c990d2b040c55bd0e5ac77b6a1a467e36b
Brandon Raitt <administrator@pxoink.net>
2015-10-23 00:22:34 -0400

    Adjust head to provide title and correct metadata.

--- head.php
+++ head.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js" data-path="<?php print(get_stylesheet_directory_uri()); ?>" />
 	<head>
-		<meta charset="utf-8" />
+		<title><?php wp_title( '|', true, 'right' ); ?></title>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1" />
 		<meta name="author" content="PxO Ink" /> <!-- Author: website void if removed. -->
 		<meta name="HandheldFriendly" content="True" />
@@ -9,6 +10,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<link rel="icon" href="<?php print(get_stylesheet_directory_uri()); ?>/images/favicon.png" />
 		<link rel="shortcut icon" href="<?php print(get_stylesheet_directory_uri()); ?>/favicon.ico" />
+		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 		<?php wp_head(); ?>
 	</head>
